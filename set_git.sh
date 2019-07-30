34a50fb7bb197b44bf63799cb5fa7d642589a25a
laygond <laygond_bryan@hotmail.com>
2019-07-30 23:17:23 +0000

    feat: Base Code

---
+++ set_git.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo Thank you Lord Beider, we now have your credentials
+git config --global user.name "laygond"
+git config --global user.email "laygond_bryan@hotmail.com"
+git remote add origin "https://github.com/laygond/Behavioral-Cloning.git"
