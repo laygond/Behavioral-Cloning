ba7cf857fea6a9c8b549e92c4535fd96d6275177
laygond <laygond_bryan@hotmail.com>
2019-08-08 02:10:48 +0000

    feat: finish training and testing

--- drive.py
+++ drive.py
@@ -11,6 +11,7 @@ import eventlet.wsgi
 from PIL import Image
 from flask import Flask
 from io import BytesIO
+import cv2
 
 from keras.models import load_model
 import h5py
@@ -64,6 +65,8 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
+        image_array = cv2.cvtColor(image_array, cv2.COLOR_RGB2BGR)
+        
         
         # Send Actuator signals
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
--- model.py
+++ model.py
@@ -1,5 +1,5 @@
-from keras.models import Sequential, Model
-from keras.layers import Flatten, Dense, Lambda
+from keras.models import Sequential, Model, load_model
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 import cv2
@@ -10,18 +10,14 @@ import matplotlib.pyplot as plt
 import os
 import csv
 import random
-
-
-samples = []
-with open('./driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        samples.append(line)
-
-train_samples, validation_samples = train_test_split(samples, test_size=0.2)
-
+from math import ceil
 
 def generator(samples, batch_size=32):
+    """
+    This is a python generator function (yields rather than return).
+    Provides features and labels in small groups (a group at a time).
+    Every group is augmented in data 2x batch_size. 
+    """
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
         random.shuffle(samples)
@@ -32,7 +28,7 @@ def generator(samples, batch_size=32):
             angles = []
             for batch_sample in batch_samples:
                 # Load feature and label
-                center_img_path = './IMG/'+batch_sample[0].split('/')[-1]
+                center_img_path = '../MYDATA03/IMG/'+batch_sample[0].split('/')[-1]
                 center_image = cv2.imread(center_img_path)
                 center_angle = float(batch_sample[3])
                 
@@ -46,57 +42,77 @@ def generator(samples, batch_size=32):
             y_train = np.array(angles)
             yield sklearn.utils.shuffle(X_train, y_train)
 
-# Set our batch size
-batch_size=32
 
-# compile and train the model using the generator function
-train_generator = generator(train_samples, batch_size=batch_size)
+            
+# READ CSV File
+samples = []
+with open('../MYDATA03/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        samples.append(line)
+
+# Load DATASET: Training and Validation         
+random.shuffle(samples)
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)            
+
+# Define generator functions
+batch_size=32
+train_generator      = generator(train_samples, batch_size=batch_size)
 validation_generator = generator(validation_samples, batch_size=batch_size)
 
-ch, row, col = 3, 80, 320  # Trimmed image format
+# #Explore generator structure 
+# gen_list = []
+# gen_list = next(train_generator)
+# print (gen_list[1][0])       # First steering angle value
+# print (gen_list[0][0].shape) # Shape of first image
+# img = cv2.cvtColor(gen_list[0][0],cv2.COLOR_BGR2RGB)
+# plt.figure()#figsize=(11, 5))   #width, height in inches
+# plt.imshow(img)
+# plt.show()
 
-model = Sequential()
-# Preprocess incoming data, centered around zero with small standard deviation 
-# model.add(Lambda(lambda x: x/127.5 - 1.,
-#         input_shape=(ch, row, col),
-#         output_shape=(ch, row, col)))
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape =(160,320,3)))
-model.add(Cropping2D(cropping=((70, 25), (0,0))))
-model.add(Convolution2D (24,5,5,subsample=(2,2),activation="relu"))
-model.add(Convolution2D (36,5,5,subsample=(2,2),activation= "relu"))
-model.add(Convolution2D (48,5,5,subsample=(2,2), activation="relu"))
-model.add(Convolution2D (64,3,3,activation="relu"))
-model.add(Convolution2D (64,3,3, activation="relu"))
-model.add(Flatten())
-model.add(Dense(100))
-model.add(Dense(50))
-model.add(Dense(10))
-model.add(Dense(1))
+# Load or Define model
+if("model.h5" in os.listdir(".")):
+    # Load Model
+    model = load_model("model.h5")
+else:
+    # Model Net
+    model = Sequential()
+    ch, row, col = 3, 160, 320
+    model.add(Cropping2D(cropping=((50, 20), (0,0)), input_shape =(row,col,ch)))
+    model.add(Lambda(lambda x: x/127.5 - 1.)) # Preprocess centered around zero with small standard deviation
+    model.add(Convolution2D (24,5,5,subsample=(2,2),activation="relu"))
+    model.add(Convolution2D (36,5,5,subsample=(2,2),activation= "relu"))
+    model.add(Convolution2D (48,5,5,subsample=(2,2), activation="relu"))
+    model.add(Convolution2D (64,3,3,activation="relu"))
+    model.add(Convolution2D (64,3,3, activation="relu"))
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
+    model.add(Dense(1))
+    model.compile(loss='mse', optimizer='adam')
 
-model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator, 
+# Train Model Net
+history_object = model.fit_generator(train_generator, 
                     steps_per_epoch=ceil(len(train_samples)/batch_size), 
                     validation_data=validation_generator, 
                     validation_steps=ceil(len(validation_samples)/batch_size), 
-                    epochs=5, verbose=1)
+                    epochs=7, verbose=1)
 
-model.save('model.h5')
+# Save model
+model.save('model2.h5')
 
-
-history_object = model.fit_generator(train_generator, samples_per_epoch =
-    len(train_samples), validation_data = 
-    validation_generator,
-    nb_val_samples = len(validation_samples), 
-    nb_epoch=5, verbose=1)
-
-### print the keys contained in the history object
+# print the keys contained in the history object
 print(history_object.history.keys())
 
-### plot the training and validation loss for each epoch
+# plot the training and validation loss for each epoch
 plt.plot(history_object.history['loss'])
 plt.plot(history_object.history['val_loss'])
 plt.title('model mean squared error loss')
 plt.ylabel('mean squared error loss')
 plt.xlabel('epoch')
 plt.legend(['training set', 'validation set'], loc='upper right')
+plt.savefig("data3", bbox_inches='tight')
 plt.show()
+
+
